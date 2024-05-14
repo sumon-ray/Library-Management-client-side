e3b19605c52d95e02de5a7683eb7238e65a8bd33
sumon <hydravulgaris60@gmail.com>
2024-05-15 00:58:00 +0600

    private route has been set up for private data.

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -75,7 +75,7 @@ export default function Banner() {
 //  
 // 
 // 
-// private route has been set up for private data.
+// 
 // error handling functionality has been added for both the login and register page 
 // 404 route has been setup. if user hit to a wrong way this page will be shown.
 //  added page has been added user can add book from this page as many as he want.
