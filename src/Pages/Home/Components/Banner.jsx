27f3c9caf59949bb959515fd8a11d50db0a2bc56
sumon <hydravulgaris60@gmail.com>
2024-05-15 00:56:11 +0600

    all book page has been added. user can see all the added book.

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -71,7 +71,7 @@ export default function Banner() {
 // 
 
 // 
-// all book page has been added. user can see all the added book.
+// 
 //  user can see the all added book in a card or a table view. 
 //  filtered book based on the quantity functionality has been implemented .
 // update functionality has been implemented for each book in the all book page.
